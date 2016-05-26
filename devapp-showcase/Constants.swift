61d9082132b9a4010b3ac69e48a4d4651e5a9bdb
Bartosz Odrzywołek <Bart@Bartoszs-MacBook-Pro.local>
2016-05-26 17:56:44 +0200

    Configure login controller

---
+++ devapp-showcase/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  devapp-showcase
+//
+//  Created by Bartosz Odrzywołek on 25/05/16.
+//  Copyright © 2016 Bartosz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
---
+++ devapp-showcase/MaterialView.swift
@@ -0,0 +1,21 @@
+//
+//  MaterialView.swift
+//  devapp-showcase
+//
+//  Created by Bartosz Odrzywołek on 25/05/16.
+//  Copyright © 2016 Bartosz. All rights reserved.
+//
+
+import UIKit
+
+class MaterialView: UIView {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = 2.0
+        layer.shadowColor = UIColor(red: SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.5).CGColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSizeMake(0.0, 2.0)
+    }
+
+}
