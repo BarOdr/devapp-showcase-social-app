a9c46c752c75a252f760ff44b6e00eaaa4b2835a
Bartosz Odrzywołek <Bart@Bartoszs-MacBook-Pro.local>
2016-05-27 18:14:05 +0200

    Implement Facebook login

---
+++ Pods/Target Support Files/Pods-devapp-showcase/Pods-devapp-showcase-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_devapp_showcaseVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_devapp_showcaseVersionString[];
+
--- devapp-showcase/ViewController.swift
+++ devapp-showcase/ViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class ViewController: UIViewController {
 
@@ -20,6 +23,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func fbBtnPressed(sender: UIButton!) {
+        let facebookLogin = FBSDKLoginManager()
+        
+//        let ref = Farebase(url: "https://devapp-showcase.firebaseio.com/")
+        facebookLogin.logInWithReadPermissions(["email"]) { (facebookResult: FBSDKLoginManagerLoginResult!, facebookError: NSError!) -> Void in
+            
+            if facebookError != nil {
+                print("Facebook login failed. Error: \(facebookError)")
+            } else if facebookResult.isCancelled {
+                print("Facebook login was cancelled")
+            } else {
+                let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
+                print("Successfuly logged in with Facebook. Access token: \(accessToken)")
+                
+//                ref.authWithOAuthProvider("facebook", token: accessToken, withCompletionBlock: { error, authData in
+                
+//            })
+                
+            }
+        
+        }
+    }
 
 }
+
+
 
